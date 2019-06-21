4c27a2637d92f8c63c076e241a21575975ed7d74
slice <ryaneft@gmail.com>
2019-06-20 23:28:52 -0700

    add tooltip to media items

--- b2/MediaCollectionViewItem.swift
+++ b2/MediaCollectionViewItem.swift
@@ -1,10 +1,23 @@
 import Cocoa
 
 class MediaCollectionViewItem: NSCollectionViewItem {
+    static func toolTipForFile(_ file: MediaFile) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale.current
+        dateFormatter.doesRelativeDateFormatting = true
+
+        let timestampHumanReadable = dateFormatter.string(from: file.metadata.timestamp)
+        let sizeHumanReadable = ByteCountFormatter.string(fromByteCount: Int64(file.metadata.size), countStyle: .file)
+        return "added \(timestampHumanReadable), \(sizeHumanReadable)"
+    }
+
     var file: MediaFile! {
         didSet {
             let thumbnailPath = self.file.path(type: .thumbnail).string
             self.imageView!.image = NSImage(byReferencingFile: thumbnailPath)
+            self.view.toolTip = MediaCollectionViewItem.toolTipForFile(self.file)
         }
     }
 

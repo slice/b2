afd3a409505dea931a4d30dd6611f6cb5a089380
slice <ryaneft@gmail.com>
2019-06-21 21:30:30 -0700

    fix image hint log spam, clean up MediaCollectionViewItem

--- b2/MediaCollectionViewItem.swift
+++ b2/MediaCollectionViewItem.swift
@@ -1,35 +1,55 @@
 import Cocoa
+import Path_swift
 
 class MediaCollectionViewItem: NSCollectionViewItem {
     override var isSelected: Bool {
         didSet {
-            self.view.layer!.borderColor = self.isSelected ? NSColor.selectedControlColor.highlight(withLevel: 0.2)!.cgColor : NSColor.clear.cgColor
-            self.view.layer!.backgroundColor = self.isSelected ? NSColor.selectedControlColor.cgColor : NSColor.quaternaryLabelColor.cgColor
+            self.view.layer!.borderColor = self.isSelected
+                ? NSColor.selectedControlColor.highlight(withLevel: 0.2)!.cgColor
+                : NSColor.clear.cgColor
+
+            self.view.layer!.backgroundColor = self.isSelected
+                ? NSColor.selectedControlColor.cgColor
+                : NSColor.quaternaryLabelColor.cgColor
 
             if self.isSelected {
-                (self.collectionView?.dataSource as? ViewController)?.currentlySelectedFile = self.file
+                let viewController = self.collectionView?.dataSource as? ViewController
+                viewController?.currentlySelectedFile = self.file
             }
         }
     }
 
-    static func toolTipForFile(_ file: MediaFile) -> String {
+    var file: MediaFile! {
+        didSet {
+            self.setupImage(path: self.file.path(type: .thumbnail))
+            self.view.toolTip = self.toolTip()
+        }
+    }
+
+    private func setupImage(path: Path) {
+        // We manually read the file instead of passing the path because
+        // thumbnails have a .thumbnail extension, which bugs out AppKit as it
+        // tries to guess what type of image it is.
+        if let data = try? Data(contentsOf: path) {
+            self.imageView!.image = NSImage(data: data)
+        } else {
+            NSLog("Cannot setup image; failed to read \(path).")
+        }
+    }
+
+    private func toolTip() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
         dateFormatter.locale = Locale.current
         dateFormatter.doesRelativeDateFormatting = true
 
-        let timestampHumanReadable = dateFormatter.string(from: file.metadata.timestamp)
-        let sizeHumanReadable = ByteCountFormatter.string(fromByteCount: Int64(file.metadata.size), countStyle: .file)
+        let timestampHumanReadable = dateFormatter.string(from: self.file.metadata.timestamp)
+        let sizeHumanReadable = ByteCountFormatter.string(
+            fromByteCount: Int64(self.file.metadata.size),
+            countStyle: .file
+        )
         return "added \(timestampHumanReadable), \(sizeHumanReadable)"
-    }
-
-    var file: MediaFile! {
-        didSet {
-            let thumbnailPath = self.file.path(type: .thumbnail).string
-            self.imageView!.image = NSImage(byReferencingFile: thumbnailPath)
-            self.view.toolTip = MediaCollectionViewItem.toolTipForFile(self.file)
-        }
     }
 
     override func mouseDown(with event: NSEvent) {

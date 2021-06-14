945f241253d450a0bb721b14c72c174c12c10916
slice <tinyslices@gmail.com>
2021-06-13 18:42:54 -0700

    wire AppDelegate to our application

--- b2/AppDelegate.swift
+++ b2/AppDelegate.swift
@@ -3,6 +3,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        NSLog("\u{1f986} quack quack")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
